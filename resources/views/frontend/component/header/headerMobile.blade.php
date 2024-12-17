860b35b973199a41f9eaeabe6e5ac20f6e03cd99
Dang Tu <anhtuhanam1@gmail.com>
2024-12-17 08:32:43 +0700

    update

--- resources/views/frontend/component/header/headerMobile.blade.php
+++ resources/views/frontend/component/header/headerMobile.blade.php
@@ -48,313 +48,38 @@
 				<ul id="menu-standard" class="">
 					<li
 						id="mobile-menu-item-626"
-						class="menu-item menu-item-type-custom menu-item-object-custom current-menu-ancestor current-menu-parent menu-item-has-children mkdf-active-item has_sub">
-						<a href="#" class="current mkdf-mobile-no-link"><span>Home</span></a><span class="mobile_arrow"><i class="mkdf-sub-arrow ion-ios-arrow-right"></i><i class="ion-ios-arrow-down"></i></span>
-						<ul class="sub_menu">
-							<li
-								id="mobile-menu-item-636"
-								class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-29 current_page_item">
-								<a href="index.html" class=""><span>Main Home</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-633"
-								class="menu-item menu-item-type-post_type menu-item-object-page">
-								<a href="alternating-posts/index.html" class=""><span>Alternating Posts</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-632"
-								class="menu-item menu-item-type-post_type menu-item-object-page">
-								<a href="travelogue-home/index.html" class=""><span>Travelogue Home</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-837"
-								class="menu-item menu-item-type-post_type menu-item-object-page">
-								<a href="travel-blog-metro/index.html" class=""><span>Travel Blog Metro</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-787"
-								class="menu-item menu-item-type-post_type menu-item-object-page">
-								<a href="floating-posts/index.html" class=""><span>Floating Posts</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-634"
-								class="menu-item menu-item-type-post_type menu-item-object-page">
-								<a href="blog-divided/index.html" class=""><span>Blog Divided</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-635"
-								class="menu-item menu-item-type-post_type menu-item-object-page">
-								<a href="travel-blog/index.html" class=""><span>Travel Blog</span></a>
-							</li>
-						</ul>
+						class="menu-item menu-item-type-custom menu-item-object-custom  {{ request()->routeIs('index') ? 'mkdf-active-item current-menu-ancestor' : '' }} current-menu-parent menu-item-has-children  has_sub">
+						<a href="{{route('index')}}" class="current mkdf-mobile-no-link"><span>SaigonTours</span></a><span class="mobile_arrow"><i class="mkdf-sub-arrow ion-ios-arrow-right"></i><i class="ion-ios-arrow-down"></i></span>
+						
 					</li>
 					<li
 						id="mobile-menu-item-627"
-						class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children has_sub">
-						<a href="#" class="mkdf-mobile-no-link"><span>Pages</span></a><span class="mobile_arrow"><i class="mkdf-sub-arrow ion-ios-arrow-right"></i><i class="ion-ios-arrow-down"></i></span>
-						<ul class="sub_menu">
-							<li
-								id="mobile-menu-item-1085"
-								class="menu-item menu-item-type-post_type menu-item-object-page">
-								<a href="about-me/index.html" class=""><span>About me</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-1225"
-								class="menu-item menu-item-type-post_type menu-item-object-page">
-								<a href="about-us/index.html" class=""><span>About us</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-1226"
-								class="menu-item menu-item-type-post_type menu-item-object-page">
-								<a href="get-in-touch/index.html" class=""><span>Get in touch</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-1179"
-								class="menu-item menu-item-type-post_type menu-item-object-page">
-								<a href="faqs/index.html" class=""><span>FAQ</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-1181"
-								class="menu-item menu-item-type-custom menu-item-object-custom">
-								<a href="error-page-404.html" class=""><span>404 error page</span></a>
-							</li>
-						</ul>
+						class="{{ request()->routeIs('booking') ? 'mkdf-active-item current-menu-ancestor' : '' }}  menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children has_sub">
+						<a href="{{route('booking')}}" class="mkdf-mobile-no-link"><span>Book Now</span></a><span class="mobile_arrow"><i class="mkdf-sub-arrow ion-ios-arrow-right"></i><i class="ion-ios-arrow-down"></i></span>
+						
 					</li>
 					<li
 						id="mobile-menu-item-628"
-						class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children has_sub">
-						<a href="#" class="mkdf-mobile-no-link"><span>Destinations</span></a><span class="mobile_arrow"><i class="mkdf-sub-arrow ion-ios-arrow-right"></i><i class="ion-ios-arrow-down"></i></span>
-						<ul class="sub_menu">
-							<li
-								id="mobile-menu-item-2087"
-								class="menu-item menu-item-type-post_type menu-item-object-page">
-								<a href="destination-list/index.html" class=""><span>Destination list</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-2564"
-								class="menu-item menu-item-type-post_type menu-item-object-page">
-								<a href="destination-slider/index.html" class=""><span>Destination slider</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-2563"
-								class="menu-item menu-item-type-post_type menu-item-object-page">
-								<a
-									href="destination-category-list/index.html"
-									class=""><span>Destination category list</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-2088"
-								class="menu-item menu-item-type-custom menu-item-object-custom">
-								<a
-									href="destination-item/colorado-2/index.html"
-									class=""><span>Destination single</span></a>
-							</li>
-						</ul>
+						class="{{ request()->routeIs('FAQ') ? 'mkdf-active-item current-menu-ancestor' : '' }} menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children has_sub">
+						<a href="{{route('FAQ')}}" class="mkdf-mobile-no-link"><span>FAQ</span></a><span class="mobile_arrow"><i class="mkdf-sub-arrow ion-ios-arrow-right"></i><i class="ion-ios-arrow-down"></i></span>
+						
 					</li>
 					<li
 						id="mobile-menu-item-629"
-						class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children has_sub">
-						<a href="#" class="mkdf-mobile-no-link"><span>Blog</span></a><span class="mobile_arrow"><i class="mkdf-sub-arrow ion-ios-arrow-right"></i><i class="ion-ios-arrow-down"></i></span>
-						<ul class="sub_menu">
-							<li
-								id="mobile-menu-item-2043"
-								class="menu-item menu-item-type-post_type menu-item-object-page">
-								<a href="masonry/index.html" class=""><span>Masonry</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-2577"
-								class="menu-item menu-item-type-post_type menu-item-object-page">
-								<a href="info-right/index.html" class=""><span>Info right</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-2586"
-								class="menu-item menu-item-type-post_type menu-item-object-page">
-								<a href="blog-carousel/index.html" class=""><span>Carousel</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-2582"
-								class="menu-item menu-item-type-post_type menu-item-object-page">
-								<a href="blog-slider/index.html" class=""><span>Slider</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-2044"
-								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children has_sub">
-								<a href="#" class="mkdf-mobile-no-link"><span>Standard</span></a><span class="mobile_arrow"><i
-										class="mkdf-sub-arrow ion-ios-arrow-right"></i><i class="ion-ios-arrow-down"></i></span>
-								<ul class="sub_menu">
-									<li
-										id="mobile-menu-item-637"
-										class="menu-item menu-item-type-post_type menu-item-object-page">
-										<a
-											href="right-sidebar/index.html"
-											class=""><span>Right sidebar</span></a>
-									</li>
-									<li
-										id="mobile-menu-item-2031"
-										class="menu-item menu-item-type-post_type menu-item-object-page">
-										<a
-											href="left-sidebar/index.html"
-											class=""><span>Left sidebar</span></a>
-									</li>
-									<li
-										id="mobile-menu-item-2034"
-										class="menu-item menu-item-type-post_type menu-item-object-page">
-										<a href="no-sidebar/index.html" class=""><span>No sidebar</span></a>
-									</li>
-								</ul>
-							</li>
-							<li
-								id="mobile-menu-item-639"
-								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children has_sub">
-								<a href="#" class="mkdf-mobile-no-link"><span>Post types</span></a><span class="mobile_arrow"><i
-										class="mkdf-sub-arrow ion-ios-arrow-right"></i><i class="ion-ios-arrow-down"></i></span>
-								<ul class="sub_menu">
-									<li
-										id="mobile-menu-item-638"
-										class="menu-item menu-item-type-post_type menu-item-object-post">
-										<a
-											href="the-laugavegur-trail/index.html"
-											class=""><span>Standard</span></a>
-									</li>
-									<li
-										id="mobile-menu-item-2069"
-										class="menu-item menu-item-type-post_type menu-item-object-post">
-										<a
-											href="a-nomads-guide-to-exploring-java/index.html"
-											class=""><span>Gallery</span></a>
-									</li>
-									<li
-										id="mobile-menu-item-2027"
-										class="menu-item menu-item-type-post_type menu-item-object-post">
-										<a
-											href="sometimes-the-hike-is-better-than-the-top-02/index.html"
-											class=""><span>Link</span></a>
-									</li>
-									<li
-										id="mobile-menu-item-2026"
-										class="menu-item menu-item-type-post_type menu-item-object-post">
-										<a
-											href="sometimes-the-hike-is-better-than-the-top/index.html"
-											class=""><span>Quote</span></a>
-									</li>
-									<li
-										id="mobile-menu-item-2059"
-										class="menu-item menu-item-type-post_type menu-item-object-post">
-										<a
-											href="amazon-rainforest-experience/index.html"
-											class=""><span>Video</span></a>
-									</li>
-									<li
-										id="mobile-menu-item-2058"
-										class="menu-item menu-item-type-post_type menu-item-object-post">
-										<a
-											href="city-that-deserve-more-than-a-stopover/index.html"
-											class=""><span>Audio</span></a>
-									</li>
-									<li
-										id="mobile-menu-item-2075"
-										class="menu-item menu-item-type-post_type menu-item-object-post">
-										<a
-											href="climbing-in-the-swiss-alps/index.html"
-											class=""><span>No sidebar</span></a>
-									</li>
-								</ul>
-							</li>
-						</ul>
+						class="{{ request()->routeIs('gallery') ? 'mkdf-active-item current-menu-ancestor' : '' }} menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children has_sub">
+						<a href="{{route('gallery')}}" class="mkdf-mobile-no-link"><span>Gallery</span></a><span class="mobile_arrow"><i class="mkdf-sub-arrow ion-ios-arrow-right"></i><i class="ion-ios-arrow-down"></i></span>
+						
 					</li>
 					<li
 						id="mobile-menu-item-630"
-						class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children has_sub">
-						<a href="#" class="mkdf-mobile-no-link"><span>Shop</span></a><span class="mobile_arrow"><i class="mkdf-sub-arrow ion-ios-arrow-right"></i><i class="ion-ios-arrow-down"></i></span>
-						<ul class="sub_menu">
-							<li
-								id="mobile-menu-item-2592"
-								class="menu-item menu-item-type-post_type menu-item-object-page">
-								<a href="product-list/index.html" class=""><span>Product list</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-963"
-								class="menu-item menu-item-type-post_type menu-item-object-product">
-								<a href="product/backpack/index.html" class=""><span>Product single</span></a>
-							</li>
-							<li
-								id="mobile-menu-item-974"
-								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children has_sub">
-								<a href="#" class="mkdf-mobile-no-link"><span>Shop layouts</span></a><span class="mobile_arrow"><i
-										class="mkdf-sub-arrow ion-ios-arrow-right"></i><i class="ion-ios-arrow-down"></i></span>
-								<ul class="sub_menu">
-									<li
-										id="mobile-menu-item-1812"
-										class="menu-item menu-item-type-post_type menu-item-object-page">
-										<a
-											href="product-list/2-columns/index.html"
-											class=""><span>2 columns</span></a>
-									</li>
-									<li
-										id="mobile-menu-item-1813"
-										class="menu-item menu-item-type-post_type menu-item-object-page">
-										<a
-											href="product-list/3-columns/index.html"
-											class=""><span>3 columns</span></a>
-									</li>
-									<li
-										id="mobile-menu-item-1814"
-										class="menu-item menu-item-type-post_type menu-item-object-page">
-										<a
-											href="product-list/4-columns/index.html"
-											class=""><span>4 columns</span></a>
-									</li>
-									<li
-										id="mobile-menu-item-1815"
-										class="menu-item menu-item-type-post_type menu-item-object-page">
-										<a
-											href="product-list/4-columns-wide/index.html"
-											class=""><span>4 columns wide</span></a>
-									</li>
-									<li
-										id="mobile-menu-item-1816"
-										class="menu-item menu-item-type-post_type menu-item-object-page">
-										<a
-											href="product-list/5-columns-wide/index.html"
-											class=""><span>5 columns wide</span></a>
-									</li>
-								</ul>
-							</li>
-							<li
-								id="mobile-menu-item-962"
-								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children has_sub">
-								<a href="#" class="mkdf-mobile-no-link"><span>Shop pages</span></a><span class="mobile_arrow"><i
-										class="mkdf-sub-arrow ion-ios-arrow-right"></i><i class="ion-ios-arrow-down"></i></span>
-								<ul class="sub_menu">
-									<li
-										id="mobile-menu-item-956"
-										class="menu-item menu-item-type-post_type menu-item-object-page">
-										<a
-											href="product-list/my-account/index.html"
-											class=""><span>My account</span></a>
-									</li>
-									<li
-										id="mobile-menu-item-958"
-										class="menu-item menu-item-type-post_type menu-item-object-page">
-										<a
-											href="product-list/cart/index.html"
-											class=""><span>Cart</span></a>
-									</li>
-									<li
-										id="mobile-menu-item-957"
-										class="menu-item menu-item-type-post_type menu-item-object-page">
-										<a
-											href="product-list/checkout-page/index.html"
-											class=""><span>Checkout page</span></a>
-									</li>
-								</ul>
-							</li>
-						</ul>
+						class="{{ request()->routeIs('about') ? 'mkdf-active-item current-menu-ancestor' : '' }} menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children has_sub">
+						<a href="{{route('about')}}" class="mkdf-mobile-no-link"><span>About Us</span></a><span class="mobile_arrow"><i class="mkdf-sub-arrow ion-ios-arrow-right"></i><i class="ion-ios-arrow-down"></i></span>
+						
 					</li>
 					<li
 						id="mobile-menu-item-1817"
-						class="menu-item menu-item-type-post_type menu-item-object-page">
-						<a href="landing/index.html" class=""><span>Landing</span></a>
+						class="{{ request()->routeIs('blog') ? 'mkdf-active-item current-menu-ancestor' : '' }} menu-item menu-item-type-post_type menu-item-object-page">
+						<a href="{{route('blog')}}" class=""><span>	Blog</span></a>
 					</li>
 				</ul>
 			</div>
